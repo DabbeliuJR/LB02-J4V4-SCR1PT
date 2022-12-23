f2793575095f775c62c8ec126f4c01dfe7c8f8b3
DabbeliuJR <Dabbeliu@outlook.de>
2022-12-23 11:16:19 +0100

    baby i dont understant it, these changes

--- script.js
+++ script.js
@@ -105,6 +105,12 @@ const btn = document.querySelector('#btn');
 const radioButtonsGender = document.querySelectorAll('input[name="gender"]');
 const radioButtonsAge = document.querySelectorAll('input[name="age"]');
 const radioButtonsHeight = document.querySelectorAll('input[name="height"]');
+const radioButtonsHair = document.querySelectorAll('input[name="hairColor"]');
+const radioButtonsEye = document.querySelectorAll('input[name="eyes"]');
+const radioButtonsHairLenght = document.querySelectorAll('input[name="hairLenght"]');
+const radioButtonsBody = document.querySelectorAll('input[name="bodybuild"]');
+
+
 
 
 
@@ -136,14 +142,55 @@ btn.addEventListener("click", () => {
         }
     }
 
+    let selectedHair;
+    for (const radioButton of radioButtonsHair) {
+        if (radioButton.checked) {
+            selectedHair = radioButton.value;
+            console.log("hair selection")
+            break;
+        }
+    }
 
-    var summe = eval(Number(selectedAge) + Number(selectedGender) + Number(selectedHeight));
+    let selectedEye;
+    for (const radioButton of radioButtonsEye) {
+        if (radioButton.checked) {
+            selectedEye = radioButton.value;
+            console.log("Eye selection")
+            break;
+        }
+    }
+
+    let selectedhairLenght;
+    for (const radioButton of radioButtonsHairLenght) {
+        if (radioButton.checked) {
+            selectedhairLenght = radioButton.value;
+            console.log("Hair Lenght selection")
+            break;
+        }
+    }
+
+    let selectedBody;
+    for (const radioButton of radioButtonsBody) {
+        if (radioButton.checked) {
+            selectedBody = radioButton.value;
+            console.log("Hair Lenght selection")
+            break;
+        }
+    }
+
+
+    var summe = eval(Number(selectedAge) + Number(selectedGender) + Number(selectedHeight) + Number(selectedHair) + Number(selectedEye) + Number(selectedhairLenght) + Number(selectedBody));
     console.log(summe);
     // show the output:
     output.innerText = summe  ? `You selected ${summe}` : `You haven't selected any size`;
-    console.log(selectedAge, selectedGender, selectedHeight);
+    console.log(selectedAge, selectedGender, selectedHeight, selectedHair, selectedEye, selectedhairLenght, selectedBody);
+    anzeige.innerText = summe  ? `You selected ${summe}` : `You haven't selected any size`;
+
 
 });
+
+
+
 
 
 // https://joergs-forum.de/in-javascript-addieren-statt-zusammenfuegen-t-2109-1
